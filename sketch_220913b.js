6deb46a6d4896be851d32624c91872e9d8ab859b
ssumoaa <33883577+ssumoa@users.noreply.github.com>
2022-11-25 02:39:49 +0900

    Update sketch_220913b.js

--- sketch_220913b.js
+++ sketch_220913b.js
@@ -1,16 +1,5 @@
-//listen, I'm not saying it's good, but I am saying it's done.
-//and probably plottable
-//and done in good fun
-//with affection.
-
-
-//Oh, Here's a version to export a SVG on click- in case anyone wanted one 
-//for plotting or stickers or whatever (BYO fill algorithm if you don't use axidraw's  inkscape extention):
-// https://editor.p5js.org/areuland/sketches/747uffXvT
-
 let kwords = ['봄', '은', '짧', '고', '하', '루', '는', '길', '다', '보', '라', '색', '바', '이', '올', '렛', '제', '비', '꽃', '흰', '상', '여', '비', '하', '얀', '벚', '꽃', '비', '박', '하', '향', '비', '읍', '취', '향'];
 let img;
-//let p1 ="봄씨는 'ㅂ' 취향이구나\n나무는 나무, 봄은 비읍\n물어보지 않아도\n히읗은 벚꽃의 취향\n봄씨는 비읍을 좋아한다 무척\n예를 들면\n바이올렛\n비\n벗꽃\n박하향\n"
 
 let p1 = ["봄 씨는 'ㅂ'취향이구나", '나무는 나무, 봄은 비읍', '물어보지 않아도', '히읗은 벚꽃의 취향', '봄씨는 비읍을 좋아한다 무척', '예를 들면', '바이올렛', '비', '벗꽃', '박하향',];
 let p2 = [ '누구누구씨는', '가슴이 뭉개진 두부처럼', 
@@ -30,13 +19,15 @@ P3 = [];
 P4 = [];
 
 function preload() {
+  shirtFont= loadFont('data/BOD_B.ttf');
+  font= loadFont('data/BOD_BI.ttf');
   kfont= loadFont('data/kopubM.otf');
   img = loadImage('data/letter.png');
 
 }
 
 function setup() {
-  createCanvas(1920, 1180);
+  createCanvas(1920, 1080);
  
 }
 
@@ -51,9 +42,10 @@ function draw() {
   textAlign(CENTER, CENTER);
   background(color('#7F00FF'));
   fill(255);
-  rect(0, 1080, width, 1180);
+  noStroke();
+  rect(0, 980, width, 100);
    imageMode(CORNER);
-   image(img,-50, -220, width, img.height*width/img.width);
+   image(img,-38, -325, width, img.height*width/img.width);
   if (frameCount % 10 == 0) {
    
     wordsOfSpring.push(new word(random(width*0.15, width*0.85), 165));
@@ -64,7 +56,8 @@ function draw() {
     wordsOfSpring[i].show();
   }
   textFont(kfont);
-  textSize(40);
+  textSize(30);
+
   smooth(); 
 
   val+=spd;
@@ -73,7 +66,7 @@ function draw() {
   if(part == 1){
     
   textAlign(LEFT);
-    text(p1[index], width*0.05, height-46);
+    text(p1[index], width*0.05, height-55);
     if (int(val)% 510 ==0){
       index ++;
     }
@@ -84,7 +77,18 @@ function draw() {
   }
   else if(part == 2){
      textAlign(CENTER);
-     text(p2[index], width*0.5, height-46);
+     text(p2[index], width*0.3, height-55);
+    if (int(val)% 510 ==0){
+      index ++;
+    }
+    if(index==9){
+      index=0;
+      part++;
+    }
+  }
+  else if(part == 3){
+     textAlign(CENTER);
+     text(p2[index], width*0.6, height-55);
     if (int(val)% 510 ==0){
       index ++;
     }
@@ -95,7 +99,7 @@ function draw() {
   }
   else{
      textAlign(RIGHT);
-     text(p3[index], width*0.95, height-46);
+     text(p4[index], width*0.95, height-55);
     if (int(val)% 510 ==0){
       index ++;
     }
